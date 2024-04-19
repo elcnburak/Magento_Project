e70ac540195c0797c916ca67ede2b5c372629be3
elcnburak <elcnburak@hotmail.com>
2024-04-19 23:37:37 +0100

    US708_Wish_List and US706_Product_Ordering runner added

--- src/test/java/Runners/US706_Product_Ordering.java
+++ src/test/java/Runners/US706_Product_Ordering.java
@@ -1,4 +1,12 @@
 package Runners;
 
-public class US706_Product_Ordering {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/java/Runners/US706_Product_Ordering.java"},
+        glue = {"StepDefinitions"}
+)
+
+public class US706_Product_Ordering extends AbstractTestNGCucumberTests {
 }
--- src/test/java/Runners/US708_Wish_List.java
+++ src/test/java/Runners/US708_Wish_List.java
@@ -1,4 +1,12 @@
 package Runners;
 
-public class US708_Wish_List {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/java/Runners/US708_Wish_List.java"},
+        glue = {"StepDefinitions"}
+)
+
+public class US708_Wish_List extends AbstractTestNGCucumberTests {
 }
